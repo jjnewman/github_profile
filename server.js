49cf0020d51d9d806e70fa9de10dea2a01772a4d
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-03-19 15:15:10 +0000

    deployed

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
   response.render('index')
 });
 
-server.listen(3000, function(){
-  console.log("Server listening on port 3000");
+server.listen(port, function(){
+  console.log("Server listening on port " + port);
 });
 
 module.exports = server;
