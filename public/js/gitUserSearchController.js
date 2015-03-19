35d67c133a2fc135f9eb7282c1fc9cb57a923b4f
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-03-19 16:05:35 +0000

    followers html

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,12 +1,13 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
  
   var searchResource = $resource('https://api.github.com/search/users');
-  // var key = '24b282ff3579e89e5f1725a41faed8375abd3575';
+  var searchRepo = $resource('https://api.github.com/users/');
 
   $scope.doSearch = function (){
     $scope.searchResult = searchResource.get({
         q: $scope.searchTerm
-        // access_token: key
+
+
       });
     };
  
